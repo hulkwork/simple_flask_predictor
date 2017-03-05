e5cba64c175805caf54a21306931007487418774
HELAL Michou <helal.michou.works@gmail.com>
2017-03-05 16:54:05 +0100

    add sms spam data

--- engine.py
+++ engine.py
@@ -1,9 +1,18 @@
 from sklearn import datasets
+import os
 from sklearn.ensemble import RandomForestClassifier
+
+
+# Iris data
 iris = datasets.load_iris()
 X = iris.data[:, :2]  # we only take the first two features.
 Y = iris.target
 
+#
+import urllib
+if 'smsspamcollection.zip' not in os.listdir('.'):
+	url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00228/smsspamcollection.zip"
+	urllib.urlretrieve(url, "smsspamcollection.zip")
 class Model(object):
 	def __init__(self,path_data='/home'):
 		self.path_data = path_data
@@ -15,6 +24,14 @@ class Model(object):
 	def predictor(self,data):
 		return self.model.predict(data)
 
-
-#m = Model()
-#print m.predictor([[0,2] for i in xrange(3)])
+class spam_Model(object):
+	def __init__(self,path_data=''):
+		self.path_data = path_data
+		self.__train()
+	def __train(self):
+		self.model = RandomForestClassifier()
+		self.model = self.model.fit(X,Y)
+		
+	def predictor(self,data):
+		return self.model.predict(data)
+	
